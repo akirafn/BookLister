cca2b9e0567b12510d64c985d882d8cc7560b3d2
akirafn <flavio.nakahara@gmail.com>
2017-06-16 10:00:44 -0300

    Criacao de modelos de dados

---
+++ BookLister/Model/Book.h
@@ -0,0 +1,13 @@
+//
+//  Book.h
+//  BookLister
+//
+//  Created by Flavio Akira Nakahara on 6/14/17.
+//  Copyright © 2017 Flavio Akira Nakahara. All rights reserved.
+//
+
+#ifndef Book_h
+#define Book_h
+
+
+#endif /* Book_h */
---
+++ BookLister/Model/Book.m
@@ -0,0 +1,9 @@
+//
+//  Book.m
+//  BookLister
+//
+//  Created by Flavio Akira Nakahara on 6/14/17.
+//  Copyright © 2017 Flavio Akira Nakahara. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
